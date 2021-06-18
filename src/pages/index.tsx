f9e03db156c13a618563a0919df95e03ae8114cd
Cadu <dudu.spgomes@gmail.com>
2021-06-18 19:25:55 -0300

    chore: add eslint, prettier, vscode settings, editorconfig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-export default function Home () {
-  return (
-    <div>Personal Boilerplate</div>
-  )
+import React from 'react'
+
+export default function Home() {
+  return <div>Personal Boilerplate</div>
 }
