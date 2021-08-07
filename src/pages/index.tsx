f3ef7bdbbfb404d7845a38a9adc592173a7e025b
Cadu <dudu.spgomes@gmail.com>
2021-08-07 19:46:31 -0300

    refactor: index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,9 @@
-import {
-  Flex,
-  Heading,
-  Icon,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  Text
-} from '@chakra-ui/react'
+import { Box, Flex, Icon, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import Copyright from 'components/copyright'
 import SearchIcon from 'components/icons/search-icon'
 import Loading from 'components/loading'
-import router from 'next/router'
+import Logo from 'components/logo'
+import SearchInput from 'components/search-input'
 import React from 'react'
 
 export default function Home() {
@@ -22,47 +16,16 @@ export default function Home() {
       flexDirection="column"
       alignItems="center"
     >
-      <Heading
-        as="h3"
-        fontFamily="Bungee Regular"
-        fontSize="4xl"
-        color="whiteAlpha.800"
-        marginTop="75px"
-      >
-        MYSUPERHERO
-      </Heading>
+      <Logo />
       <InputGroup marginY="auto" w="343px" h="51px">
         <InputLeftElement pointerEvents="none" h="100%">
           <Icon as={SearchIcon} />
         </InputLeftElement>
-        <Input
-          type="text"
-          h="100%"
-          border="1px solid"
-          borderColor="#313542"
-          bgColor="#1E222F"
-          color="#BBBBBB"
-          fontFamily="Gotham Regular"
-          fontSize="sm"
-          fontWeight="700"
-          onKeyUp={(e: React.KeyboardEvent<HTMLInputElement>) => {
-            const name = e.currentTarget.value
-            if (e.key === 'Enter') {
-              router.push(`/search?name=${name}`)
-            }
-          }}
-        />
+        <SearchInput />
       </InputGroup>
-      <Text
-        fontFamily="Roboto Regular"
-        color="white"
-        fontSize="xs"
-        fontWeight="400"
-        letterSpacing=".4px"
-        marginBottom="19px"
-      >
-        Data provided by Marvel. © 2020 MARVEL
-      </Text>
+      <Box marginBottom="19px">
+        <Copyright />
+      </Box>
       <Loading />
     </Flex>
   )
